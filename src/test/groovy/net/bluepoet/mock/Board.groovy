3dea1ec449276f8dfce15efd1d600e0d0a105c19
bluepoet <bluepoet1112@gmail.com>
2016-11-30 01:08:52 +0900

    add test case

---
+++ src/test/groovy/net/bluepoet/mock/Board.groovy
@@ -0,0 +1,8 @@
+package net.bluepoet.mock
+
+/**
+ * Created by bluepoet on 2016. 11. 30..
+ */
+interface Board {
+    String write(content)
+}
--- src/test/groovy/net/bluepoet/mock/ClassMockTest.groovy
+++ src/test/groovy/net/bluepoet/mock/ClassMockTest.groovy
@@ -21,4 +21,16 @@ class ClassMockTest extends Specification {
         { verify(user, times(1)).hello() }
         result == 'mock hello'
     }
+
+    def "interface mocking뒤 실행 후, 검증성공을 확인한다."() {
+        given:
+        Board board = Mock()
+        board.write('test content') >> 'write ok!'
+
+        when:
+        board.write('test content')
+
+        then:
+        1 * board.write('test content')
+    }
 }
